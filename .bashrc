b2761951ee1569b0f2d819f6966923908245103e
onederey <morozovcustom@yandex.ru>
2025-09-28 18:04:10 +0300

    небольшой тюнинг, переход на pywal 16

--- .bashrc
+++ .bashrc
@@ -5,8 +5,20 @@
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
+# -- base --
 alias ls='ls -a --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 
-source ~/scripts/setup.sh
+#if [ -f "$HOME/.cache/wal/colors.sh" ]; then
+#  . "$HOME/.cache/wal/colors.sh"
+#fi
+
+# -- pywal --
+source ~/.cache/wal/colors-tty.sh
+(cat ~/.cache/wal/sequences &)
+
+# -- alias --
+alias hyprlandconf='vim ~/.config/hypr/hyprland.conf'
+alias zzz='shutdown -h now'
+
--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -6,7 +6,7 @@
 
 sudo -v
 
-packages=( firefox fastfetch kitty telegram-desktop obsidian qbittorrent vlc vim git keepassxc less dotnet-sdk base-devel brightnessctl waybar hyprpaper otf-font-awesome hyprlock stow hypridle python-pywal ttf-jetbrains-mono ttf-jetbrains-mono-nerd pavucontrol blueman ) 
+packages=( firefox fastfetch kitty telegram-desktop obsidian qbittorrent vlc vim git keepassxc less dotnet-sdk base-devel brightnessctl waybar hyprpaper otf-font-awesome hyprlock stow hypridle ttf-jetbrains-mono ttf-jetbrains-mono-nerd pavucontrol blueman ) 
 
 echo  "Listed packages will be installed:"
 echo  "	-> ${packages[@]}"
@@ -20,6 +20,6 @@ makepkg -si
 cd ..
 rm -rf ./yay
 
-aur=( visual-studio-code-bin amneziavpn-bin python-pywalfox wlogout )
+aur=( visual-studio-code-bin amneziavpn-bin python-pywal16 python-pywalfox wlogouti )
 
 yay -Sy "${aur[@]}"
